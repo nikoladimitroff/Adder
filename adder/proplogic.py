8954e933f8affb4e1fd4d82a1d510fed4d688fea
Nikola DimitrofF <dimitroff.nikola@gmail.com>
2014-05-31 01:58:50 +0300

    CNF Refactoring

--- adder/proplogic.py
+++ adder/proplogic.py
@@ -8,8 +8,79 @@ class LogicOperator:
     Negation = "!"
     Implication = "=>"
     Equivalence = "<=>"
-    LeftBrace = "("
-    RightBrace = ")"
+
+
+class _Braces:
+    Left = "("
+    Right = ")"
+    Placeholder = "*"
+    
+    @staticmethod
+    def remove_surrounding_parenthesis(sentence):    
+        if sentence[0] == _Braces.Left and \
+           sentence[-1] == _Braces.Right:
+            braces = 1
+            for symbol in sentence[1:-1]:
+                if symbol == _Braces.Left: braces += 1
+                if symbol == _Braces.Right: braces -= 1
+                if braces == 0: return sentence
+            return sentence[1:-1]
+        return sentence
+    
+    @staticmethod
+    def flatten_braces(text):
+        tlb = False
+        braces = 0
+        result = ""
+        for symbol in text:
+            if symbol == _Braces.Left:
+                if braces == 0:
+                    tlb = True
+                    result += _Braces.Left
+                braces += 1
+            elif symbol == _Braces.Right:
+                braces -= 1
+                if braces == 0:
+                    tlb = False
+                    result += _Braces.Right
+            else:
+                result += symbol
+            
+        return result
+
+    def brace_replace(text):
+        braces = 0
+        tlb = False
+        result = ""
+        replacement_table = []
+        replacement_index = -1
+        for symbol in text:
+            if tlb and (braces != 1 or symbol != _Braces.Right):
+                result += _Braces.Placeholder
+                replacement_table[replacement_index] += symbol
+            else:
+                result += symbol
+            
+            if symbol == _Braces.Left:
+                if braces == 0:
+                    tlb = True
+                    replacement_table.append(_Braces.Left)
+                    replacement_index += 1
+                braces += 1
+            elif symbol == _Braces.Right:
+                braces -= 1
+                if braces == 0:
+                    tlb = False
+                    replacement_table[replacement_index] += _Braces.Right
+
+        return (re.sub("\*+", _Braces.Placeholder, result), replacement_table)
+    
+    def restore_braces(text, replacement_table):
+        for entry in replacement_table:
+            text = text.replace("({0})".format(_Braces.Placeholder), entry, 1)
+    
+        return text
+
 
 class DefiniteClause:
     def __init__(self, text):
@@ -76,23 +147,23 @@ def forward_chaining(knowledge_base, query):
 def backward_chaining(knowledge_base, query):
     true_symbols = {clause.conclusion for clause in knowledge_base if clause.is_fact}
 
-    return _backward_chaining_step(knowledge_base, query, true_symbols, true_symbols)
+    return __backward_chaining_step(knowledge_base, query, true_symbols, true_symbols)
 
-def _backward_chaining_step(kb, query, true_symbols, checked):
+def __backward_chaining_step(kb, query, true_symbols, checked):
     if query in true_symbols:
         return True
 
     applicable_implications = (clause for clause in kb 
                                if query == clause.conclusion)
 
-    return any(_check_clause(kb, true_symbols, checked, clause) 
+    return any(__check_clause(kb, true_symbols, checked, clause) 
                for clause in applicable_implications)
 
-def _check_clause(kb, true_symbols, checked, clause):
+def __check_clause(kb, true_symbols, checked, clause):
     all = True
     for premise in clause.premises:
         if premise not in checked:
-            is_premise_true = _backward_chaining_step(kb, premise, true_symbols, 
+            is_premise_true = __backward_chaining_step(kb, premise, true_symbols, 
                                                 checked.union({premise}))
             all = all and is_premise_true
     return all
@@ -100,106 +171,111 @@ def _check_clause(kb, true_symbols, checked, clause):
 def resolution_prover(knowledge_base, query):
     pass
 
-def _remove_surrounding_parenthesis(sentence):    
-    if sentence[0] == LogicOperator.LeftBrace and \
-       sentence[-1] == LogicOperator.RightBrace:
-        braces = 1
-        for symbol in sentence[1:-1]:
-            if symbol == LogicOperator.LeftBrace: braces += 1
-            if symbol == LogicOperator.RightBrace: braces -= 1
-            if braces == 0: return sentence
-        return sentence[1:-1]
-    return sentence
 
-def flatten_braces(text):
-    tlb = False
-    braces = 0
-    result = ""
-    for symbol in text:
-        if symbol == "(":
-            if braces == 0:
-                tlb = True
-                result += "("
-            braces += 1
-        elif symbol == ")":
-            braces -= 1
-            if braces == 0:
-                tlb = False
-                result += ")"
-        else:
-            result += symbol
-            
-    return result
-
-def equivalence_cnf(operands):
+def __equivalence_cnf(operands):
     lhs, rhs = operands
-    cnf = _convert_to_cnf("({2}{0} {3} {1}) {4} ({2}{1} {3} {0})"
+    cnf = __convert_to_cnf("({2}{0} {3} {1}) {4} ({2}{1} {3} {0})"
                             .format(lhs, rhs, 
                                     LogicOperator.Negation, 
                                     LogicOperator.Disjunction, 
                                     LogicOperator.Conjuction))
     return cnf
 
-def implication_cnf(operands):
+
+def __implication_cnf(operands):
     lhs, rhs = operands
-    cnf = _convert_to_cnf("{2}{0} {3} {1}".format(lhs, rhs, LogicOperator.Negation, LogicOperator.Disjunction))
+    cnf = __convert_to_cnf("{2}{0} {3} {1}".format(lhs, rhs, 
+                                                   LogicOperator.Negation, 
+                                                   LogicOperator.Disjunction))
     return cnf    
 
-def disjunction_cnf(operands):
+
+def __disjunction_cnf(operands):
     lhs, rhs = operands
-    first = _convert_to_cnf(lhs).split(LogicOperator.Conjuction)
-    second = _convert_to_cnf(rhs).split(LogicOperator.Conjuction)
+    first = __convert_to_cnf(lhs).split(LogicOperator.Conjuction)
+    second = __convert_to_cnf(rhs).split(LogicOperator.Conjuction)
     cnf = ""
     for clause1 in first:
         for clause2 in second:
-            cnf += "({0} {2} {1}) {3} ".format(clause1, clause2, LogicOperator.Disjunction, LogicOperator.Conjuction)
+            cnf += "({0} {2} {1}) {3} ".format(clause1, clause2, 
+                                               LogicOperator.Disjunction, 
+                                               LogicOperator.Conjuction)
 
     return cnf[:-2]
 
-def conjuction_cnf(operands):
+
+def __conjuction_cnf(operands):
     lhs, rhs = operands
-    cnf = "{0} {2} {1}".format(_convert_to_cnf(lhs), _convert_to_cnf(rhs), LogicOperator.Conjuction)
+    cnf = "{0} {2} {1}".format(__convert_to_cnf(lhs), 
+                               __convert_to_cnf(rhs), 
+                               LogicOperator.Conjuction)
     return cnf
 
-def negation_cnf(operands):
-    tail = _remove_surrounding_parenthesis(operands[0])
-    if is_var(tail):
+
+def __negation_cnf(operands):
+    tail = _Braces.remove_surrounding_parenthesis(operands[0])
+    if __is_var(tail):
         return LogicOperator.Negation + tail
 
     tail_operation = parse_sentence(tail)
     operator = tail_operation[0]
     tail_operands = tail_operation[1:]
+    rewritten_formula = ""
     if operator == LogicOperator.Negation:
-        return _convert_to_cnf(tail_operands[0])
+        rewritten_formula = tail_operands[0]
     elif operator == LogicOperator.Conjuction:
         lhs, rhs = tail_operands
-        return _convert_to_cnf("{2}{0} {3} {2}{1}".format(lhs, rhs, LogicOperator.Negation, LogicOperator.Disjunction))
+        rewritten_formula = "{2}{0} {3} {2}{1}".format(lhs, rhs, 
+                                                       LogicOperator.Negation, 
+                                                       LogicOperator.Disjunction)
     elif operator == LogicOperator.Disjunction:
         lhs, rhs = tail_operands
-        return _convert_to_cnf("{2}{0} {3} {2}{1}".format(lhs, rhs, LogicOperator.Negation, LogicOperator.Conjuction))
+        rewritten_formula = "{2}{0} {3} {2}{1}".format(lhs, rhs, 
+                                                       LogicOperator.Negation, 
+                                                       LogicOperator.Conjuction)
     else:
-        return _convert_to_cnf("{0}({1})".format(LogicOperator.Negation, _convert_to_cnf(tail)))
+        rewritten_formula = "{0}({1})".format(LogicOperator.Negation,
+                                              __convert_to_cnf(tail))
 
-OPERATOR_PRECEDENCE = {
-    LogicOperator.Equivalence: equivalence_cnf,
-    LogicOperator.Implication: implication_cnf,
-    LogicOperator.Disjunction: disjunction_cnf,
-    LogicOperator.Conjuction: conjuction_cnf,
-    LogicOperator.Negation: negation_cnf
+    return __convert_to_cnf(rewritten_formula)
+
+__OPERATOR_PARSERS = {
+    LogicOperator.Equivalence: __equivalence_cnf,
+    LogicOperator.Implication: __implication_cnf,
+    LogicOperator.Disjunction: __disjunction_cnf,
+    LogicOperator.Conjuction: __conjuction_cnf,
+    LogicOperator.Negation: __negation_cnf
 }
 
-def is_disjunct_true(disjunct):
+def parse_cnf_clause(sentence):
+    cnf = _Braces.flatten_braces(__convert_to_cnf(sentence))
+   
+    clauses = []
+    for disjunct_text in cnf.split(LogicOperator.Conjuction):
+        disjunct_text = disjunct_text.replace(_Braces.Left, "") \
+                            .replace(_Braces.Right, "")
+        if len(disjunct_text) == 0:
+            continue
+
+        disjunct = {symbol.strip() for symbol in 
+                    disjunct_text.split(LogicOperator.Disjunction)}
+
+        if not __is_disjunct_true(disjunct):
+            clauses.append(disjunct)
+
+    return __clean_clauses(clauses)
+
+def __is_disjunct_true(disjunct):
     return any(LogicOperator.Negation + symbol in disjunct 
                for symbol in disjunct)
 
-def clean_clauses(clauses):
+def __clean_clauses(clauses):
     unique = []
     for clause in clauses:
         if clause not in unique:
             unique.append(clause)
     
     result = []
-
     for clause in unique:
         is_superset = any([conjuct < clause for conjuct in unique])
         if not is_superset:
@@ -207,31 +283,9 @@ def clean_clauses(clauses):
 
     return result
 
-def parse_cnf_clause(sentence):
-    cnf = flatten_braces(_convert_to_cnf(sentence))
-   
-    clauses = []
-    for disjunct_text in cnf.split(LogicOperator.Conjuction):
-        disjunct_text = disjunct_text.replace(LogicOperator.LeftBrace, "") \
-                .replace(LogicOperator.RightBrace, "")
-        if len(disjunct_text) == 0:
-            continue
 
-        disjunct = {symbol.strip() for symbol in 
-                    disjunct_text.split(LogicOperator.Disjunction)}
-
-        if not is_disjunct_true(disjunct):
-            clauses.append(disjunct)
-
-    return clean_clauses(clauses)
-
-def print_cnf_clause(clause):
-    printable = "(" +") & (".join([" | ".join(disjunct) for disjunct in clause]) + ")"
-    return printable
-
-
-def _convert_to_cnf(sentence):
-    # Match every pair of ()
+def __convert_to_cnf(sentence):
+    # Match every pair of (). Substitue with *
     # For each of them, call recursively the same func
     # If the sentence has no parenthesis, swap a <=> b with (!a | b) & (!b | a)
     # If the sentence contains implication, swap a => b with (!a | b)
@@ -239,16 +293,15 @@ def _convert_to_cnf(sentence):
     # Use distrubutivity
     
 
-    if is_var(sentence):
-        result = sentence
-        return result
+    if __is_var(sentence):
+        return sentence
 
     parsed = parse_sentence(sentence)
-    result = OPERATOR_PRECEDENCE[parsed[0]](parsed[1:])
+    result = __OPERATOR_PARSERS[parsed[0]](parsed[1:])
     return result
 
 
-def is_var(sentence):
+def __is_var(sentence):
     return not (LogicOperator.Negation in sentence or \
            LogicOperator.Conjuction in sentence or \
            LogicOperator.Disjunction in sentence or \
@@ -257,14 +310,14 @@ def is_var(sentence):
 
 def parse_sentence(sentence):
     # remove surrounding parenthesis
-    sentence = _remove_surrounding_parenthesis(sentence.strip())
-    brace_replaced = brace_replace(sentence)
-    operator = get_operator(brace_replaced[0])
-    operands = get_operands(brace_replaced, operator)
+    sentence = _Braces.remove_surrounding_parenthesis(sentence.strip())
+    brace_replaced = _Braces.brace_replace(sentence)
+    operator = __get_operator(brace_replaced[0])
+    operands = __get_operands(brace_replaced, operator)
 
     return (operator, ) + operands
 
-def get_operator(brace_replaced_sentence):
+def __get_operator(brace_replaced_sentence):
     operator_precedence = [
         LogicOperator.Equivalence,
         LogicOperator.Implication,
@@ -278,11 +331,11 @@ def get_operator(brace_replaced_sentence):
             return operator
 
 
-def get_operands(brace_replaced_sentence, operator):
+def __get_operands(brace_replaced_sentence, operator):
     result, replacements = brace_replaced_sentence
 
     if operator == LogicOperator.Negation:
-        sentence = restore_braces(result, replacements)
+        sentence = _Braces.restore_braces(result, replacements)
         if sentence[0] == operator:
             return (sentence[1:], )
         return False
@@ -292,38 +345,10 @@ def get_operands(brace_replaced_sentence, operator):
     lhs, rhs = result.split(" {0} ".format(operator), maxsplit=1)
     separator = "!SEPARATOR!"
     separated = "{0} {1} {2}".format(lhs, separator, rhs)
-    lhs, rhs = restore_braces(separated, replacements).split(separator)
+    lhs, rhs = _Braces.restore_braces(separated, replacements).split(separator)
     return (lhs.strip(), rhs.strip())
 
-def brace_replace(text):
-    braces = 0
-    tlb = False
-    result = ""
-    replacement_table = []
-    replacement_index = -1
-    for symbol in text:
-        if tlb and (braces != 1 or symbol != ")"):
-            result += "*"
-            replacement_table[replacement_index] += symbol
-        else:
-            result += symbol
-            
-        if symbol == "(":
-            if braces == 0:
-                tlb = True
-                replacement_table.append("(")
-                replacement_index += 1
-            braces += 1
-        elif symbol == ")":
-            braces -= 1
-            if braces == 0:
-                tlb = False
-                replacement_table[replacement_index] += ")"
 
-    return (re.sub("\*+", "*", result), replacement_table)
-    
-def restore_braces(text, replacement_table):
-    for entry in replacement_table:
-        text = text.replace("(*)", entry, 1)
-    
-    return text
+def print_cnf_clause(clause):
+    printable = "(" +") & (".join([" | ".join(disjunct) for disjunct in clause]) + ")"
+    return printable
